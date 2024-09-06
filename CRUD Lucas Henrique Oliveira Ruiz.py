89d8bbad73258d0e567b0021e226a2e6e822650c
Lucas Ruiz <lucashiki8@gmail.com>
2024-09-06 12:49:06 -0300

    finalizaçãosem4

--- CRUD Lucas Henrique Oliveira Ruiz.py
+++ CRUD Lucas Henrique Oliveira Ruiz.py
@@ -5,18 +5,13 @@ Lucas Henrique Oliveira Ruiz | Análise e Desenvolvimento de Sistemas
 
 
 #Criação de uma variavel para armazenar a listagem de alunos 
-
 estudantes = [] 
 
 # Primeiro while verificando uma condição verdadeira no primeiro menu
 
-    #Print do menu principal
-
-
 while True:
 
     #Print do menu principal
-
     print ("---- MENU PRINCIPAL ----\n")
     print ("[1] Gerenciar Estudantes.")
     print ("[2] Gerenciar Disciplinas.")
@@ -26,14 +21,13 @@ while True:
     print ("[9] Sair\n")
 
     #Pergunta ao usuario qual a opção deseja dentro do menu principal
-
     opcao = int(input("Informe a opção desejada: "))
 
-    
+    if opcao == 9:# Condição para sair do programa
+        print ("\nFinalizando a aplicação...")
+        break
 
     #Segundo While verifica uma condição verdadeira no menu de operações
-
-
     while True:
         if opcao == 1:
             print ("\n\n***** [ESTUDANTES] MENU DE OPERAÇÕES *****\n")             
@@ -46,10 +40,12 @@ while True:
         elif opcao == 4:
             print ("\n\nEM DESENVOLVIMENTO\n")
             break
-        elif opcao == 9: ###! NÃO PODE ESTÁR DENTRO DO SEGUNDO WHILE, PQ NÃO FINALIZA A APLICAÇÃO.
-            break   
+        elif opcao == 5:
+            print("\n\nEM DESENVOLVIMENTO\n")
+            break
         else:
-            print("\n\nOpção inválida") 
+            print("\n\nOpção inválida")# Sai do segundo loop para voltar ao menu principal
+            break 
 
         #Print do menu secúndário após perguntar a condição acima, a condição dita o cabeçalho.
 
@@ -67,25 +63,26 @@ while True:
 
         if acao == 1: #Condição inclui cada aluno digitado na lista da variavel estudantes = []
             print("\n\n===== INCLUSÃO =====\n")
-            nome = input("Informe o nome do Estudante: \nPressione ENTER para continuar")
+            nome = input("Informe o nome do Estudante: ")
+            print("Pressione ENTER para continuar")
+            input()
             estudantes.append(nome)
         elif acao == 2: #Condição printa os alunos que estão dentro da lista estudantes = [], caso não tenha alunos cadastrados será printado a frase "Não há estudantes cadastrados."
             print("\n\n===== LISTAGEM =====\n")
             for i in estudantes:
                 print (i)
+            print("\nPressione ENTER para continuar")
+            input()
             if len(estudantes) == 0:
-                    print ("Não há estudantes cadastrados.")    
+                    print ("Não há estudantes cadastrados.")  
         elif acao == 3:
             print("\n\nEM DESENVOLVIMENTO\n")
             break
         elif acao == 4:
             print("\n\nEM DESENVOLVIMENTO\n")
             break
-        elif acao == 9:
+        elif acao == 9: # Voltar ao menu principal
             print("\n\nVocê voltou para o menu principal\n")
             break
         else:
             print("\n\nOpção inválida!")
-
-#Finaliza o código
-print ("Finalizando a aplicação...")
